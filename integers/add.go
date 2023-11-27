73573e5be9104252b1045b488746638111abfa3a
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-11-26 18:27:56 -0800

    * update the integers Test and function * add iteration with for loop in go

--- integers/add.go
+++ integers/add.go
@@ -1,5 +1,5 @@
-package main
+package integers
 
-func Add(x, y, int) int {
+func Add(x, y int) int {
 	return x + y
 }
---
+++ iteration/go.mod
@@ -0,0 +1,3 @@
+module github.com/abhishekamralkar/learnGoWithTests/iteration
+
+go 1.21.1
