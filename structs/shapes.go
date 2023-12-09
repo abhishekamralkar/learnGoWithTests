a397c878dc0c8fbfd03141d66796b8bc48d45b97
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-12-09 14:22:43 -0800

    [golang] add interface example

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,10 +1,25 @@
 package shapes
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width float64, height float64) float64 {
+type Circle struct {
+	Radius float64
+}
 
-	return width * height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,8 +2,16 @@ package shapes
 
 import "testing"
 
+/*
+type Rectangle struct {
+	Width  float64
+	Height float64
+        }
+*/
+
 func TestShapes(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -11,11 +19,33 @@ func TestShapes(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+areaTests := []struct{
+	name string
+	got  Area
+	want float64
+}{
+	{name: "Rectangle", got: rectangle.Area(), want:= 72.0}
+}
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+func TestArea(t *testing.T) {
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f but want %.2f", got, want)
+		}
+	})
+
+	t.Run("cirlces", func(t *testing.T) {
+		circle := Circle.Area()
+		got := Area(circle)
+		want := 314.16
+
+		if got != want {
+			t.Errorf("got %.2f but want %.2f", got, want)
+		}
+	})
 }
